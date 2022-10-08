4bcb0719eef739686adf9725dba6fb647e091b0b
Francisco Orozco <fsantam5bosch@gmail.com>
2022-10-08 20:41:52 +0100

    Agregando ForDemo de unidad 1

---
+++ Ejercicios/Ud1/ForDemo.java
@@ -0,0 +1,14 @@
+/*
+    Uso del bucle for
+    Asigne el nombre ForDemo.java a este archivo.
+*/
+class ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for(count = 0; count < 5; count = count +1 ) // Este bucle itera cinco veces.
+            System.out.println("This is count: " + count);
+
+        System.out.println("done!");
+    }
+}
